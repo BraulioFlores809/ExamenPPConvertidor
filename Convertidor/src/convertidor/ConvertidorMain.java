869e045e93c0f2d62714b4d90522582e4c1cca14
Alumno <Alumno@LDS49>
2016-09-26 13:40:13 +0000

    Comit inicial

---
+++ Convertidor/src/convertidor/ConvertidorMain.java
@@ -0,0 +1,11 @@
+package convertidor;
+
+public class ConvertidorMain {
+
+    public static void main(String[] args) {
+        
+        Converditor conv = new Converditor();
+		conv.setVisible(true);
+    }
+    
+}
